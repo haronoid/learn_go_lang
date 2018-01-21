f04e0e5d3d558d7cb47f05d3c6c066c1c558c83e
w-sung <w-sung@upset1.onmicrosoft.com>
2018-01-21 21:36:13 +0900

    update

--- zoo/main.go
+++ zoo/main.go
@@ -51,6 +51,71 @@ func main() {
 
 	myFunc() // => "内部定数スコープ"
 	MyFunc() // => "公開定数スコープ"
+
+	fruits := [3]string{"apple", "banana", "cherry"}
+
+	for i, s := range fruits {
+		fmt.Printf("Fruits[%d]=%s\n", i, s) // => "apple" "banana" "cherry"
+	}
+
+	for i, r := range "👪BC" {
+		fmt.Printf("[%d] -> %d\n", i, r)
+	}
+
+	s := "A"
+
+	switch s {
+	case "A":
+		s += "1"
+		// gallthroughがない場合は、ここで処理が終了する。
+	case "B":
+		s += "2"
+		fallthrough
+	case "C":
+		s += "3"
+		fallthrough
+	default:
+	}
+	fmt.Println(s) // A => "A1", B ==> "B23"
+
+	anything("abc") // => "abc"
+	anything(1.5)   // => "1.5"
+	anything("日本語") // => "日本語"
+	anything('a')   // => "97"
+
+	var x interface{} = 3
+
+	i, isInt := x.(int) // i == 3, isInt == true
+	fmt.Printf("%d %t\n", i, isInt)
+
+R:
+
+	switch v := x.(type) {
+	case bool:
+		fmt.Println("x is bool")
+	case int:
+		fmt.Println("x is int")
+	case string:
+		fmt.Println("x is string")
+	default:
+		fmt.Printf("x is %#v\n", v)
+	}
+
+	if x == 3 {
+		x = 4
+		goto R
+	}
+
+	// panicが起きる処理より前じゃないと動かない
+	defer func() {
+		fmt.Println("recover")
+		if x := recover(); x != nil {
+			fmt.Println(x)
+		}
+	}()
+
+	runDefer()
+
 }
 
 // 戻り値を関数にする
@@ -72,4 +137,21 @@ func callFunc(f func()) {
 	f()
 	f()
 	fmt.Println()
+}
+
+func anything(a interface{}) {
+	fmt.Println(a)
+}
+
+func runDefer() {
+
+	// deferは関数処理の最後に実行される処理, 一周のFinally処理
+
+	defer fmt.Println("defer1")
+	defer fmt.Println("defer2")
+	panic("runtime error") // throw new Exception
+
+	defer fmt.Println("defer3")
+
+	fmt.Println("runDefer Done")
 }
