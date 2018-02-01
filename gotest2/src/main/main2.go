5515efd43ce073830d5712de6626f3eb130a2e89
w-sung <w-sung@upset1.onmicrosoft.com>
2018-02-01 20:14:59 +0900

    type struct

--- gotest2/src/main/main2.go
+++ gotest2/src/main/main2.go
@@ -34,4 +34,23 @@ func main2() {
 	s += p4[0] + p4[1] + p4[2]
 	fmt.Println(s)
 
+	ip := &i
+	fmt.Printf("Type=%T Address=%p Value=%d\n", ip, ip, *ip)
+
+	mi := MyInt{A: 4, B: 5}
+	mi.calc()
+
+	fmt.Println(mi)
+
+}
+
+//type MyInt int
+type MyInt struct {
+	A   int
+	B   int
+	SUM int
+}
+
+func (m *MyInt) calc() {
+	m.SUM = m.A + m.B
 }
